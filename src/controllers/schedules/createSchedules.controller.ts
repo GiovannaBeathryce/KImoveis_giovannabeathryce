945a242e4ebe446d869ec7f58ba4a337ed92441c
giovannabeathryce <giovannabeathryce.pso@gmail.com>
2022-10-31 13:31:08 -0300

    correção de erro controller

--- src/controllers/schedules/createSchedules.controller.ts
+++ src/controllers/schedules/createSchedules.controller.ts
@@ -4,14 +4,19 @@ import { IScheduleRequest } from "../../interfaces/schedules";
 import createScheduleServices from "../../services/schedules/createSchedules.services";
 
 const createScheduleController = async (req: Request, res: Response) => {
-  const { scheduleReq } = req.body;
+  const { date, hour, userId, propertyId } = req.body;
 
   let token = req.headers.authorization;
 
   if (!token) {
     throw new AppError(401, "Invalid token");
   }
-  const createSchedule = await createScheduleServices(scheduleReq);
+  const createSchedule = await createScheduleServices({
+    date,
+    hour,
+    userId,
+    propertyId,
+  });
 
   return res.status(201).json({ message: "Scheduling performed" });
 };
